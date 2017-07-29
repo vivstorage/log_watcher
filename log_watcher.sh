cace7124973016705616f00d289a6e796756e032
vivstorage <vivstorage@gmail.com>
2017-07-29 16:13:56 +0300

    Update log_watcher.sh

--- log_watcher.sh
+++ log_watcher.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
+#mailx package required
 
 cnt_fail=0
 cnt_ok=0
-mail=0
+mail_sent=false
 i=0
+FILE=$1
 EMAIL=$2
 
-if [ -z $1 ] || [ $1 == '--help' ] || [ $1 == '-help' ] || [ $1 == '--about' ] || [ $1 == '-about' ];
+if [ -z $FILE ] || [ $FILE == '--help' ] || [ $FILE == '-help' ] || [ $FILE == '--about' ] || [ $FILE == '-about' ];
  then
  echo 'please specify log file path, ex log_watcher.sh <file path> <alert email>'
  exit 1
-elif [ ! -f $1 ]; then
- echo "$1 file not exist"
+elif [ ! -f $FILE ]; then
+ echo "$FILE file not exist"
  exit 1
 fi
 
-if [ -z $2 ]; then
+if [ -z $EMAIL ]; then
  echo 'alerts email was not specified'
  exit 1
 fi
 
-tail -Fn0 $1 |  \
+tail -Fn0 $FILE |  \
 while read LINE;
 do
  ((i = `echo $LINE | awk '{print $4}'`))
  if [ $i -gt 10 ];
   then
    ((cnt_fail = cnt_fail + 1))
-   if [ $cnt_fail -ge 3 ] && [ $mail -eq 0 ];
+   if [ $cnt_fail -ge 3 ] && [ ! $mail_sent ];
    then
-     ((mail = 1))
+     ((mail_sent = true))
      mail -s "NOTIFICATION: DOWN" $EMAIL
      #echo "send mail about problem $EMAIL"
      ((cnt_fail = 0))
@@ -37,14 +39,14 @@ do
   elif [ $i -le 10 ];
   then
     ((cnt_fail = 0))
-    if [ $mail -eq 1 ];
+    if [ $mail_sent ];
     then
       ((cnt_ok = cnt_ok + 1))
       if [ $cnt_ok -ge 3 ];
       then
         #echo "send mail about ok $EMAIL"
         mail -s "NOTIFICATION: UP" $EMAIL
-        ((mail = 0))
+        ((mail_sent = false))
       fi
     else
      continue
